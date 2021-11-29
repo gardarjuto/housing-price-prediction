c2da6b9e86cabf754839b263f403c834d4ed756b
Garðar Ingvarsson <gi241@cam.ac.uk>
2021-11-29 13:10:41 +0000

    Added data dl and sql support

--- housingpricetools/access.py
+++ housingpricetools/access.py
@@ -1,11 +1,5 @@
 from .config import *
 
-"""These are the types of import we might expect in this file
-import httplib2
-import oauth2
-import mongodb
-import sqlite"""
-
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
@@ -13,4 +7,43 @@ import sqlite"""
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
+
+
+def execute_sql(conn, sql, fetch=None):
+    """Executes the provided SQL statement given the connection, returning one, all or no rows."""
+    with conn:
+        with conn.cursor() as crs:
+            crs.execute(sql)
+            if fetch == 'one':
+                return crs.fetchone()
+            elif fetch == 'all':
+                return crs.fetchall()
+    
+
+def upload_local_file(conn, file_path, table_name):
+    """Uploads a file from the local file system into the provided table."""
+    with conn:
+        with conn.curson() as crs:
+            sql = f"""
+                LOAD DATA LOCAL INFILE '{file_path}' INTO TABLE `{table_name}`
+                FIELDS TERMINATED BY ','
+                LINES STARTING BY '' TERMINATED BY '\n';
+                """
+            crs.execute(sql)
+
+
+def download_file(url, dest_path, chunk_size=8192):
+    """Downloads a file from URL to the destination path. Creates directories if output path doesn't exist."""
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    file_name = os.path.basename(url)
+    file_path = os.path.join(dest_path, file_name)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size):
+                f.write(chunk)
+    return file_name
 

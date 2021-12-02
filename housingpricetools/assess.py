2145ea10eac6834242e28b47c0b9c05ec9fa4ffc
Garðar Ingvarsson <gi241@cam.ac.uk>
2021-12-02 13:39:42 +0000

    Added querying and data cleanup functions

--- housingpricetools/assess.py
+++ housingpricetools/assess.py
@@ -1,15 +1,38 @@
 from .config import *
 
-from . import assess
-
-"""These are the types of import we might expect in this file
 import pandas
-import bokeh
 import matplotlib.pyplot as plt
 import sklearn.decomposition as decomposition
-import sklearn.feature_extraction"""
 
-"""Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
+
+def query_prices_latlon_date(conn, min_lat, max_lat, min_lon, max_lon, start_date, end_date):
+    """
+    Queries Price Paid database table for all datapoints within latitude and longitude range and the start and end date.
+    Converts the rows into a pandas DataFrame and returns it. Irrelevant columns are excluded such as unique ids and object 
+    names since they don't have predictive power.
+    """
+    sql = f"""
+    SELECT price, date_of_transfer, property_type, latitude, longitude
+    FROM (
+        SELECT postcode, price, date_of_transfer, property_type, new_build_flag, tenure_type, street, locality, town_city, district, county, ppd_category_type, record_status
+        FROM property_prices.pp_data
+	    WHERE date_of_transfer BETWEEN '{start_date.strftime('%Y-%m-%d')}' AND '{end_date.strftime('%Y-%m-%d')}'
+        ) pp 
+    JOIN property_prices.postcode_data pd 
+    ON pd.postcode = pp.postcode
+    WHERE latitude >= {min_lat} AND latitude <= {max_lat} AND longitude >= {min_lon} AND longitude <= {max_lon};
+    """
+    df = pd.read_sql(sql, conn)
+    return df
+
+
+def clean_prices_df(df):
+  """
+  Cleans DataFrame by filtering out rows with missing values and converting the date to datetime format. 
+  """
+  df_clean = df.dropna(inplace=True)
+  df_clean.date_of_transfer = pd.to_datetime(df_clean.date_of_transfer)
+  return df
 
 
 def data():
